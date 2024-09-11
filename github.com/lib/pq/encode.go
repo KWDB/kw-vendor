8a59a628baf47b52925a61bbe0388a73963b41f0
muguanxue <muguanxue@inspur.com>
2024-09-11 09:53:36 +0800

    Revert "fix ZDP-41389【gb18030】【kwbase client】查询varchar类型gb18030字符显示为unicode编码"
    This reverts commit faa52c42f8cccb7b748e5d31915741a300d47ccb.

--- github.com/lib/pq/encode.go
+++ github.com/lib/pq/encode.go
@@ -93,9 +93,6 @@ func binaryDecode(parameterStatus *parameterStatus, s []byte, typ oid.Oid) inter
 func textDecode(parameterStatus *parameterStatus, s []byte, typ oid.Oid) interface{} {
 	switch typ {
 	case oid.T_char, oid.T_varchar, oid.T_text:
-		if parameterStatus.clientEncoding == "gbk" || parameterStatus.clientEncoding == "gb18030" {
-			return s
-		}
 		return string(s)
 	case oid.T_bytea, oid.T_varbytea:
 		b, err := parseBytea(s)
